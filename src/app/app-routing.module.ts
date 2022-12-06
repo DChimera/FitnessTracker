9c9040a5f5aee7c5971454697c63645e0b37c60b
jense <jensenconnor@ymail.com>
2022-12-06 14:51:04 -0500

    Connors Stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponentComponent } from "./about-component/about-component.component";
+import { ActivityJournalComponent } from "./activity-journal/activity-journal.component";
+import { FoodJournalComponent } from "./food-journal/food-journal.component";
+import { HomepageComponentComponent } from "./homepage-component/homepage-component.component";
+import { LoginComponentComponent } from "./login-component/login-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', component: LoginComponentComponent},
+  {path:'login', component: LoginComponentComponent},
+  {path:'about', component: AboutComponentComponent},
+  {path:'activity', component: ActivityJournalComponent},
+  {path:'home', component: HomepageComponentComponent},
+  {path:'food', component: FoodJournalComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { HomepageComponentComponent } from './homepage-component/homepage-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { FoodJournalComponent } from './food-journal/food-journal.component';
+import { ActivityJournalComponent } from './activity-journal/activity-journal.component';
+import { RoutingComponentComponent } from './routing-component/routing-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    HomepageComponentComponent,
+    AboutComponentComponent,
+    FoodJournalComponent,
+    ActivityJournalComponent,
+    RoutingComponentComponent
   ],
   imports: [
     BrowserModule,
