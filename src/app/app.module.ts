8f3bc814c1aa3da5dd812f5fb8fc5f83b93d1d35
Christopher Magnus <minisungam@gmail.com>
2022-12-06 15:38:14 -0500

    organize folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
-import { HomepageComponentComponent } from './homepage-component/homepage-component.component';
-import { AboutComponentComponent } from './about-component/about-component.component';
-import { FoodJournalComponent } from './food-journal/food-journal.component';
-import { ActivityJournalComponent } from './activity-journal/activity-journal.component';
-import { RoutingComponentComponent } from './routing-component/routing-component.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { HomepageComponentComponent } from './components/homepage-component/homepage-component.component';
+import { AboutComponentComponent } from './components/about-component/about-component.component';
+import { FoodJournalComponent } from './components/food-journal/food-journal.component';
+import { ActivityJournalComponent } from './components/activity-journal/activity-journal.component';
+import { RoutingComponentComponent } from './components/routing-component/routing-component.component';
 
 @NgModule({
   declarations: [
